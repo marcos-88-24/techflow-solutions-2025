344d42a5b576f994c71dc3248317719a0b611f53
MARCOS DOS SANTOS <mrcssantos174@gmail.com>
2026-01-04 15:46:58 +0000

    float: Atualização de produto do estoque closes #2

--- Código.py
+++ Código.py
@@ -1,11 +1,37 @@
+import os
 
-elif (opcao == '1'):
-    nome_do_produto = input('Digite o nome do produto: ')
-    preço_do_produto = float(input('Digite o preço do produto: '))
-    quantidade_em_estoque = input('Digite quantidade em estoque: ')
-    produto = {
-        "Nome": nome_do_produto,
-        "Preço": preço_do_produto,
-        "Quantidade em estoque": quantidade_em_estoque
-    }
-    estoque.append(produto)
+estoque = []
+
+def limpar_tela():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def pausar():
+    input('presione ENTRE para continuar...')    
+
+def mostrar_produtos():
+        contador = 1
+        for item in estoque:
+            print (f'{contador} - {item}')
+            contador += 1    
+
+def selecionar_menu(opcao):
+    if (opcao == '1'):
+        nome_do_produto = input('Digite o nome do produto: ')
+        preço_do_produto = float(input('Digite o preço do produto: '))
+        quantidade_em_estoque = input('Digite quantidade em estoque: ')
+        produto = {"Nome": nome_do_produto, "Preço": preço_do_produto, "Quantidade em estoque": quantidade_em_estoque}
+        estoque.append(produto)
+
+
+
+        
+    elif (opcao == '2'):
+        mostrar_produtos()
+        numero_produto = int(input('Digite o número do produto para editar: '))
+        nome_do_produto = input ('Alterar o nome do produto: ')
+        preço_do_produto = float(input('Alterar o preço do produto: '))
+        quantidade_em_estoque = input('Alterar a quantidade em estoque: ')
+        novo_produto = {"Nome": nome_do_produto, "Preco": preço_do_produto, "Quantidade em estoque": quantidade_em_estoque}
+        estoque[numero_produto -1] = novo_produto
+
+        
